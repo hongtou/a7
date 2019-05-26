caf9291de034802b8038b05d6afc398b23f26736
zht <imhongtao@163.com>
2019-05-26 18:25:28 +0800

    'commit1111'

--- src/main/java/ccc/aaa/A.java
+++ src/main/java/ccc/aaa/A.java
@@ -4,6 +4,15 @@ package ccc.aaa;/*
  */
 
 public class A {
+   int a5=0;
+
+    public int getA5() {
+        return a5;
+    }
+
+    public void setA5(int a5) {
+        this.a5 = a5;
+    }
 
     public int getDd() {
         return dd;
